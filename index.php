5e61b8c4d8b6bb0f1f1a7084045a6024f95dd500
Henry Harris <henrythomasharris@gmail.com>
2017-05-23 17:44:03 -0400

    Initial commit

--- index.php
+++
@@ -1,3 +0,0 @@
-suh dood <br>
-yes, this website is open sourced, check it out <a href="http://github.com/dummycode/Website">here</a> <br>
-<a href="http://github.com/dummycode/TicTacToe">ma first project</a>
