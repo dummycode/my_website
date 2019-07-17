a99cd750cddadda106dc7ebd4930c2f430832419
Henry Harris <henrythomasharris@gmail.com>
2019-07-17 17:14:06 -0400

    Made some progress on projects page

--- beta/projects/index.php
+++ beta/projects/index.php
@@ -2,6 +2,7 @@
 <html>
   <head>
     <title>Henry Harris - Projects</title>
+    <link rel="stylesheet" href="/beta/assets/bootstrap.min.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i&amp;subset=cyrillic,cyrillic-ext,greek,greek-ext,latin-ext,vietnamese">
     <link rel="stylesheet" href="/beta/assets/style.css">
@@ -22,7 +23,55 @@
   <body>
     <div id="sidebar" class="sidebar"></div>
 
-    <div class="body">
+    <div class="body projects">
+      <div card="container projects-favorites">
+        <div class="row">
+          <div class="col-sm-12 col-md-6 col-lg-4 project-card">
+            <div class="project-card__container">
+              <div class="project-card__title">Quick Queue</div>
+              <div class="project-card__description">This is a description of the Quick Queue project. It should be no longer than 200 characters.</div>
+              <div class="project-card__languages">
+                <div class="language-tag">JavaScript</div>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-12 col-md-6 col-lg-4 project-card">
+            <div class="project-card__container">
+              <div class="project-card__title">Quick Queue</div>
+              <div class="project-card__description">This is a description of the Quick Queue project. It should be no longer than 200 characters.</div>
+              <div class="project-card__languages">
+                <div class="language-tag">JavaScript</div>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-12 col-md-6 col-lg-4 project-card">
+            <div class="project-card__container">
+              <div class="project-card__title">Quick Queue</div>
+              <div class="project-card__description">This is a description of the Quick Queue project. It should be no longer than 200 characters.</div>
+              <div class="project-card__languages">
+                <div class="language-tag">JavaScript</div>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-12 col-md-6 col-lg-4 project-card">
+            <div class="project-card__container">
+              <div class="project-card__title">Quick Queue</div>
+              <div class="project-card__description">This is a description of the Quick Queue project. It should be no longer than 200 characters.</div>
+              <div class="project-card__languages">
+                <div class="language-tag">JavaScript</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="projects-list">
+        <ul>
+          <li>Quick Queue</li>
+          <li>My Website</li>
+          <li>Musuem Mayhem</li>
+          <li>Tic Tac Toe</li>
+        </ul>
+      </div>
       Projects need to go here
     </div>
     <div id="sidebar-footer--mobile" class="sidebar-footer--mobile"></div>
