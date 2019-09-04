a6714827a48dc78462c49465b0b4d5704d777924
Henry Harris <henrythomasharris@gmail.com>
2019-09-04 10:46:34 -0400

    Fix error page

--- 404.php
+++ 404.php
@@ -1,22 +1,32 @@
 <!DOCTYPE html>
 <html>
-   <head>
+  <head>
     <title>Henry Harris</title>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+    <link rel="stylesheet" href="/assets/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i&amp;subset=cyrillic,cyrillic-ext,greek,greek-ext,latin-ext,vietnamese">
     <link rel="stylesheet" href="/assets/style.css">
+
+    <script src="/assets/js/jquery-3.4.1.min.js"></script>
+    <script src="/assets/js/handlebars.runtime-v4.1.2.js"></script>
+    <script src="/assets/js/templatesCompiled.js"></script>
+    <script>
+      const sidebar = Handlebars.templates['sidebar/sidebar']({})
+      const sidebarFooterMobile = Handlebars.templates['footer/mobile']({})
+
+      $(document).ready(function () {
+        $("#sidebar").html(sidebar)
+        $("#sidebar-footer--mobile").html(sidebarFooterMobile)
+      })
+    </script>
   </head>
   <body>
-    <div class="container">
-      <div class="content">
-        <i class="fas fa-exclamation-triangle main-icon"></i>
-        <p>That page was not found. Click <a href="/">here</a> to return to the homepage.</p>
-      </div>
-      <div class="footer">
-        <p>
-          <a href="mailto:mail@henryharr.is">mail@henryharr.is</a>
-        </p>
+    <div id="sidebar" class="sidebar"></div>
+    <div class="body">
+      <div class="error">
+        <i class="fas fa-exclamation-triangle error__icon"></i>
+        <p class="error__message">That page was not found. Click <a href="/">here</a> to return to the homepage.</p>
       </div>
     </div>
+    <div id="sidebar-footer--mobile" class="sidebar-footer--mobile"></div>
   </body>
 </html>
