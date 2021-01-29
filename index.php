9a2b8d05d0b79ae01376dc2095f9fef68f8fc314
Henry Harris <hharris@palantir.com>
2021-01-29 10:39:46 -0500

    Update current description

--- index.php
+++ index.php
@@ -37,12 +37,8 @@
           <h1>Me</h1>
         </div>
         <p>
-          Hi, my name is Henry Harris. I am currently a 4th year Computer Science student at the Georgia Institute of Technology, better known as Georgia Tech.
-          I am also a candidate for a minor in Computing and Business through the Denning Technology and Management program.
-          At school, most of my time outside of schoolwork is dedicated to being a teaching assistant for CS 2110 (Computer Organization and Programming).
-          <!--
-              This is my website. I created this website to showcase my work and plus, all famous people have their own websites.
-              But, correlation does not equal causation. Or was it causation does not equal correlation. Oh how I wish my 10th grade statistics teacher could see me now. Anyways, who is Henry Harris? -->
+          Hi, my name is Henry Harris. I am currently a software engineer at Palantir Technologies. Previously, I received my Bachelor of Science in Computer Science from the Georgia Institute of Technology, better known as Georgia Tech.
+          At Palantir, most of my time is dedicated to working on Palantir Gotham's suite of artificial intelligence (AI) tools.
         </p>
       </div>
       <div id="story" class="section">
@@ -55,14 +51,18 @@
           The moment I saw the words "Hello World" pop up in my console, I knew programming was for me.
         </p>
         <p>
-          Since, I have taken AP Computer Science, chosen Computer Science as my major, gotten heavily involved in the College of Computing at Georgia Tech, worked three software engineering internships, built countless personal projects, and I continue to love it just as much to this day.
+          Since, I have taken AP Computer Science, chosen Computer Science as my major, gotten heavily involved in the College of Computing at Georgia Tech, interned at three software companies, started my first full-time software engineering job, built countless personal projects, and I continue to love it just as much to this day.
         </p>
       </div>
       <div id="hobbies" class="section">
         <div class="section__title">
           <h1>Hobbies</h1>
         </div>
-        <p>In my free time I enjoy exercising and the great outdoors -- specifically running, hiking, and climbing. I also love to travel the world. My favorite trip ever is the trip to Morocco I took while studying abroad in Barcelona during the summer of 2018. There, we visited Fes, toured the Atlas Mountains, rode camels into the Sahara desert where we spent the night in traditional Berber tents, played Moroccan drums with locals in Tingher Province, and navigated the bustling markets of Marrakech.</p>
+        <p>
+          In my free time I enjoy exercising and the great outdoors -- specifically running, cycling, climbing, mountaineering, and skiing.
+          I also love to travel the world. My favorite trip ever is the trip to Morocco I took while studying abroad in Barcelona during the summer of 2018.
+          There, we visited Fes, toured the Atlas Mountains, rode camels into the Sahara desert where we spent the night in traditional Berber tents, played Moroccan drums with locals in Tingher Province, and navigated the bustling markets of Marrakech.
+        </p>
       </div>
     </div>
     <div id="sidebar-footer--mobile" class="sidebar-footer--mobile"></div>
