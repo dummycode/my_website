ae59ee0f13fb91f08d3e56a676911560aa9a5754
Henry Harris <hharris@palantir.com>
2021-05-25 08:57:16 -1000

    Versionize the summits

--- climbing/index.php
+++ climbing/index.php
@@ -30,7 +30,8 @@
     <script src="https://d3js.org/d3.v3.min.js"></script>
 
     <script type="text/javascript"charset="utf-8">
-        d3.text("summits.csv", function(data) {
+        const version = 1;
+        d3.text("summits.csv" + "?v" + version, function(data) {
             const parsedCSV = d3.csv.parseRows(data);
             const summits = parsedCSV;
 
