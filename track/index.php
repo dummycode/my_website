bbd557ce81c6e7fd1d8d3b5286acca163ad4d66e
Henry Harris <hharris@palantir.com>
2021-09-02 16:52:05 -0600

    Add tracking to website

---
+++ track/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Henry Harris - Track</title>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-149253046-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-149253046-1');
+    </script>
+    <script>
+      window.location.replace("https://share.garmin.com/henryharris");
+    </script>
+  </head>
+  <body>
+    <h1>You should not see this... Visit <a href="https://share.garmin.com/henryharris">.</h1>
+  </body>
+</html>
+
