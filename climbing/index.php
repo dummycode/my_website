e0a42581ebcd39c4b4af60ed0e47a338bf3b03bf
Henry Harris <henrythomasharris@gmail.com>
2022-01-14 14:55:29 -0700

    Add running to climbing page (#9)
    Co-authored-by: Henry Harris <hharris@palantir.com>

--- climbing/index.php
+++ climbing/index.php
@@ -47,10 +47,26 @@
             });
         });
 
+        d3.text("ultras.csv" + "?v" + version, function(data) {
+            const parsedCSV = d3.csv.parseRows(data);
+            const ultras = parsedCSV;
+
+            const table = d3.select("#ultras")[0][0];
+
+            ultras.forEach(function (ultra) {
+                const tr = table.insertRow(-1);
+                ultra.forEach(function (data) {
+                    var td = document.createElement('td');
+                    td = tr.insertCell(-1);
+                    td.innerHTML = data;
+                });
+            });
+        });
+
         // Sort table
-        function sortSummits(n, isDate = false) {
+        function sortTable(tableName, n, isDate = false) {
             let i, x, y, shouldSwitch, switchcount = 0;
-            const table = document.getElementById("summits");
+            const table = document.getElementById(tableName);
             let switching = true;
             let dir = "asc";
 
@@ -104,28 +120,49 @@
     <div class="body">
       <div class="section">
         <div class="section__title">
-          <h1>Climbing</h1>
-        </div>
-        <p>I have had the opportunity to climb some of the world's most popular, difficult, and fun mountains. Here is a list of some of the hills I have been fortunate enough to summit.</p>
-      </div>
-
-      <div class="section">
-        <div class="section__title">
           <h1>Summits</h1>
         </div>
+        <p>
+            I have had the opportunity to climb some of the world's most popular, difficult, and fun mountains.
+            Here is a list of some of the hills I have been fortunate enough to summit.
+        </p>
         <table id="summits" class="summits">
             <thead>
                 <tr>
-                    <th onclick="sortSummits(0)">Summit</th>
-                    <th onclick="sortSummits(1)">Country</th>
-                    <th onclick="sortSummits(2)">Elevation (m)</th>
-                    <th onclick="sortSummits(3, true)">Date</th>
+                    <th onclick="sortTable('summits', 0)">Summit</th>
+                    <th onclick="sortTable('summits', 1)">Country</th>
+                    <th onclick="sortTable('summits', 2)">Elevation (m)</th>
+                    <th onclick="sortTable('summits', 3, true)">Date</th>
                 </tr>
             </thead>
             <tbody>
             </tbody>
         </table>
       </div>
+
+      <div class="section">
+        <div class="section__title">
+          <h1>Running</h1>
+        </div>
+        <p>
+            I have also been tirelessly running some of the best trails in the world in hopes of pushing my body to the limit.
+            Here are the top ultra distance routes I have defeated. Well, rather, those that have defeated me.
+        </p>
+        <table id="ultras" class="ultras">
+            <thead>
+                <tr>
+                    <th onclick="sortTable('ultras', 0)">Name</th>
+                    <th onclick="sortTable('ultras', 1)">Country</th>
+                    <th onclick="sortTable('ultras', 2)">Distance (km)</th>
+                    <th onclick="sortTable('ultras', 3)">Vertical (m)</th>
+                    <th onclick="sortTable('ultras', 4, true)">Date</th>
+                </tr>
+            </thead>
+            <tbody>
+            </tbody>
+        </table>
+      </div>
+
     <div id="sidebar-footer--mobile" class="sidebar-footer--mobile"></div>
   </body>
 </html>
